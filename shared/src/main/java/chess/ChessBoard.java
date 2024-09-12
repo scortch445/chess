1efbefa7fecf70286e20a916c59d84cc565b12f0
Ben Skinner <ben.h.skinner@gmail.com>
2024-09-12 10:10:30 -0600

    added base code, started work on chessPosition, chessPiece, and chessBoard

---
+++ shared/src/main/java/chess/ChessBoard.java
@@ -0,0 +1,50 @@
+package chess;
+
+/**
+ * A chessboard that can hold and rearrange chess pieces.
+ * <p>
+ * Note: You can add to this class, but you may not alter
+ * signature of the existing methods.
+ */
+public class ChessBoard {
+
+    private final ChessPosition[][] boardSpots = new ChessPosition[10][10];
+    private ChessPiece[][] piecesOnBoard = new ChessPiece[10][10];
+
+    public ChessBoard() {
+        for(int i=0; i< boardSpots.length; i++){
+            for(int j=0; i<boardSpots[i].length; i++){
+                boardSpots[i][j] = new ChessPosition(i,j);
+            }
+        }
+    }
+
+    /**
+     * Adds a chess piece to the chessboard
+     *
+     * @param position where to add the piece to
+     * @param piece    the piece to add
+     */
+    public void addPiece(ChessPosition position, ChessPiece piece) {
+        piecesOnBoard[position.getColumn()][position.getRow()] = piece;
+    }
+
+    /**
+     * Gets a chess piece on the chessboard
+     *
+     * @param position The position to get the piece from
+     * @return Either the piece at the position, or null if no piece is at that
+     * position
+     */
+    public ChessPiece getPiece(ChessPosition position) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Sets the board to the default starting board
+     * (How the game of chess normally starts)
+     */
+    public void resetBoard() {
+        throw new RuntimeException("Not implemented");
+    }
+}
---
+++ shared/src/main/java/chess/ChessPiece.java
@@ -0,0 +1,57 @@
+package chess;
+
+import java.util.Collection;
+
+/**
+ * Represents a single chess piece
+ * <p>
+ * Note: You can add to this class, but you may not alter
+ * signature of the existing methods.
+ */
+public class ChessPiece {
+
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
+    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
+    }
+
+    /**
+     * The various different chess piece options
+     */
+    public enum PieceType {
+        KING,
+        QUEEN,
+        BISHOP,
+        KNIGHT,
+        ROOK,
+        PAWN
+    }
+
+    /**
+     * @return Which team this chess piece belongs to
+     */
+    public ChessGame.TeamColor getTeamColor() {
+        return this.pieceColor;
+    }
+
+    /**
+     * @return which type of chess piece this piece is
+     */
+    public PieceType getPieceType() {
+        return this.type;
+    }
+
+    /**
+     * Calculates all the positions a chess piece can move to
+     * Does not take into account moves that are illegal due to leaving the king in
+     * danger
+     *
+     * @return Collection of valid moves
+     */
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        throw new RuntimeException("Not implemented");
+    }
+}

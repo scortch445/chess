fa399dd1d4c1fd06b899b53f57db8d6738e5577f
Ben Skinner <ben.h.skinner@gmail.com>
2024-10-17 16:08:48 -0600

    setting up tests and organizing code

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -3,8 +3,6 @@ import server.Server;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-
         Server server = new Server();
         System.out.println("♕ 240 Chess Server: " + server.run(8080));
     }
---
+++ server/src/test/java/service/ServiceTests.java
@@ -0,0 +1,22 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ServiceTests {
+    // I have to implement this
+    // Options: @BeforeAll, @BeforeEach, @AfterEach, @AfterAll
+    // @Test
+
+    // DataAccess should use @ParameterizedTest with @ValueSource
+
+    // Use Run with Coverage to make sure all code is run and tested
+
+
+    @Test
+    void clear(){
+        assertTrue(true);
+        // First value is the expected value, second is what we're checking
+        assertEquals(true, 1==1);
+    }
+}

59ce1b9510d1106261a793c966a6a9b6dbfb89e2
Ben Skinner <ben.h.skinner@gmail.com>
2024-10-29 11:48:03 -0600

    Phase 4 - Pet Shop helper functions ported over

--- server/src/main/java/dataaccess/SqlDataAccess.java
+++ server/src/main/java/dataaccess/SqlDataAccess.java
@@ -3,10 +3,20 @@ package dataaccess;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
+import server.ServerException;
 
 import java.util.ArrayList;
 
+import java.sql.*;
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
 public class SqlDataAccess implements DataAccess {
+
+    public SqlDataAccess() throws ServerException {
+        configureDatabase();
+    }
+
     @Override
     public UserData getUser(String username) {
         return null;
@@ -56,4 +66,65 @@ public class SqlDataAccess implements DataAccess {
     public void clear() {
 
     }
+
+    private int executeUpdate(String statement, Object... params) throws ServerException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) {
+                        ps.setString(i + 1, p);
+                    }
+                    else if (param instanceof Integer p) {
+                        ps.setInt(i + 1, p);
+                    }
+                    else if (param instanceof Object o) {
+                        ps.setString(i + 1, o.toString());
+                    }
+                    else if (param == null) {
+                        ps.setNull(i + 1, NULL);
+                    }
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new ServerException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  pet (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `name` varchar(256) NOT NULL,
+              `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(type),
+              INDEX(name)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    private void configureDatabase() throws ServerException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new ServerException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+
 }

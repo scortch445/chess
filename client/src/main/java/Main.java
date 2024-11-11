8c3f11af29621cd13ee70aa962b5f30ec8be3f44
Ben Skinner <ben.h.skinner@gmail.com>
2024-11-11 13:50:02 -0700

    Phase 5 - First REPL made

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,42 @@
 import chess.*;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private enum State {
+        PRELOGIN,
+        POSTLOGIN,
+        INGAME
+    }
+
+    private static State state;
+    private static Scanner scanner;
+
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+
+        state = State.PRELOGIN;
+        scanner = new Scanner(System.in);
+
         System.out.println("♕ 240 Chess Client: " + piece);
+
+        runREPL();
+    }
+
+    private static void runREPL(){
+        while(true){
+            if(state==State.PRELOGIN){
+                System.out.print("[LOGGED OUT] >>> ");
+                String output = scanner.nextLine();
+
+                System.out.println("Inputted: " + output);
+
+            } else if (state==State.POSTLOGIN) {
+
+            } else if (state==State.INGAME){
+
+            }
+        }
     }
 }
